8758f160a3b8802924eae7438de2969d939dab3e
Mellow1213 <dpcks7282@gmail.com>
2024-12-16 16:02:50 +0900

    Feat: "Detect Camera collide obstacle"

--- Camera/PlayerCamera.cs
+++ Camera/PlayerCamera.cs
@@ -9,16 +9,22 @@ namespace SG
 
         public Camera cameraObject;
         private Transform target;
-        
+        [SerializeField] private Transform cameraPivotTransform;
         [Header("Camera Setting")] [SerializeField]
         private Vector3 cameraVelocity;
-
+        
         [SerializeField] private float minClamp = -60.0f;
         [SerializeField] private float maxClamp = 75.0f;
         [SerializeField] private float cameraSmoothSpeed = 1;
         [SerializeField] private Transform rightPivot;
         [SerializeField] private Transform upPivot;
         public float sensitivity = 0.1f;
+        [SerializeField] private float cameraCollisionPivot = 0.2f;
+        private Vector3 cameraObjectPosition;
+        private float cameraZPosition;
+        private float targetCameraZPosition;
+        private float cameraCollisionRadius = 0.2f;
+        [SerializeField] private LayerMask collideWithLayers;
 
         private void Awake()
         {
@@ -35,6 +41,7 @@ namespace SG
         void Start()
         {
             DontDestroyOnLoad(this);
+            cameraZPosition = cameraObject.transform.localPosition.z;
         }
 
         public void SetFollowTarget(Transform t)
@@ -49,6 +56,7 @@ namespace SG
             // Rotate Around Player by Mouse
              RotatePlayer();
             // Block by Obstacle (like wall)
+            CheckCollision();
         }
 
         void FollowPlayer()
@@ -69,7 +77,29 @@ namespace SG
             rotationY = Mathf.Clamp(rotationY, minClamp, maxClamp);
             rightPivot.localRotation = Quaternion.Euler(0, rotationX, 0);
             upPivot.localRotation = Quaternion.Euler(-rotationY, 0, 0);
-            Debug.Log(vec);
+        }
+
+        void CheckCollision()
+        {
+            targetCameraZPosition = cameraZPosition;
+            RaycastHit hit;
+            Vector3 direction = cameraObject.transform.position - cameraPivotTransform.position;
+            Debug.DrawRay(cameraPivotTransform.position, direction, Color.red);
+            direction.Normalize();
+            if (Physics.SphereCast(cameraPivotTransform.position, cameraCollisionRadius, direction, out hit,
+                    Mathf.Abs(targetCameraZPosition), collideWithLayers))
+            {
+                float distanceFromHitObject = Vector3.Distance(cameraPivotTransform.position, hit.point);
+                targetCameraZPosition = -(distanceFromHitObject - cameraCollisionRadius);
+            }
+
+            if (Mathf.Abs(targetCameraZPosition) < cameraCollisionRadius)
+            {
+                targetCameraZPosition = -cameraCollisionRadius;
+            }
+
+            cameraObjectPosition.z = Mathf.Lerp(cameraObject.transform.localPosition.z, targetCameraZPosition, 0.2f);
+            cameraObject.transform.localPosition = cameraObjectPosition;
         }
     }
 }
--- Character/Player/PlayerLocomotionManager.cs
+++ Character/Player/PlayerLocomotionManager.cs
@@ -14,10 +14,13 @@ public class PlayerLocomotionManager : CharacterLocomotionManager
     public float currentSpeed;
     [SerializeField] private float rotateSpeed = 15.0f;
 
+    private Transform cameraT;
+    
     protected override void Awake()
     {
         base.Awake();
         player = GetComponent<PlayerManager>();
+        
     }
 
     protected override void Update()
@@ -33,16 +36,12 @@ public class PlayerLocomotionManager : CharacterLocomotionManager
     private void GroundMovement()
     {
         Vector2 inputVector = InputManager.instance.GetMove();
-        
         Vector3 forward = PlayerCamera.instance.cameraObject.transform.forward * inputVector.y;
         Vector3 right = PlayerCamera.instance.cameraObject.transform.right * inputVector.x;
         moveVec = (forward + right).normalized;
         moveVec.y = 0;
-        Debug.Log("moveVec = " + moveVec);
-
-        currentSpeed = isRun ? walkSpeed : runSpeed;
+        currentSpeed = isRun ? runSpeed : walkSpeed;
         _characterController.Move(moveVec * (currentSpeed * Time.deltaTime));
-//        Debug.Log(moveVec);
         if (moveVec != Vector3.zero)
         {
             Quaternion targetRotation = Quaternion.LookRotation(moveVec);
